1297f2afa083e5d23bd6cfa1c37e2da27b38beed
gabriel <155402760+gabriel-pixel35@users.noreply.github.com>
2024-06-27 00:33:04 -0300

    Delete frontend/src/components/__dashboard directory

--- frontend/src/components/__dashboard/index.js
+++
@@ -1,5 +0,0 @@
-export { CardStatus } from "./CardStatus";
-export { Navbar } from "./Navbar";
-export { UserTable } from "./Table";
-export { TaskCard } from "./TaskCard";
-export { Kanban } from "./Kanban";
