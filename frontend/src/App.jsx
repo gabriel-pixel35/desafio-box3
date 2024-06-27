21fe7ebb91dc8f2d55acc04298dd88fd1788a8b3
gabriel <155402760+gabriel-pixel35@users.noreply.github.com>
2024-06-27 00:32:28 -0300

    Delete frontend/src/App.jsx

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { AuthProvider } from "./context/AuthContext";
-import { AppRouter } from "./routes/PublicRoutes";
-export function App() {
-	return (
-		<AuthProvider>
-			<AppRouter />
-		</AuthProvider>
-	);
-}
