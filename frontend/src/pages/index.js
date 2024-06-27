dcf8ca6db9bcf9999359a8f49ef4a6cd1ac54596
gabriel <155402760+gabriel-pixel35@users.noreply.github.com>
2024-06-27 00:31:57 -0300

    Delete frontend/src/pages/index.js

--- frontend/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-// Exporta o componente Dashboard do arquivo "./Dashboard"
-export { Dashboard } from "./Dashboard";
-
-// Exporta o componente TableDashboard do arquivo "./TableDashboard"
-export { TableDashboard } from "./TableDashboard";
-
-// Exporta o componente Login do arquivo "./Login"
-export { Login } from "./Login";
